b2336b51dbbd6d0302d34068f49c8e0720e280d8
Ivan Doronin <ivandoronin280@gmail.com>
2021-10-16 23:20:46 +1100

    Fix export statements for entites and value objects

---
+++ auth_kit/core_auth_kit/lib/src/domain/entities/core_auth_entities.dart
@@ -0,0 +1,3 @@
+library core_auth_entites;
+
+export 'package:core_auth_kit/src/domain/entities/user.dart';
---
+++ auth_kit/core_auth_kit/lib/src/domain/value_objects/core_auth_value_objects.dart
@@ -0,0 +1,6 @@
+library core_auth_value_objects;
+
+export 'package:core_auth_kit/src/domain/value_objects/email.dart';
+export 'package:core_auth_kit/src/domain/value_objects/name.dart';
+export 'package:core_auth_kit/src/domain/value_objects/password.dart';
+export 'package:core_auth_kit/src/domain/value_objects/uid.dart';
