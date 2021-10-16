10b90181914d7d46bec8c2c348be55bcfa7894c2
Ivan Doronin <ivandoronin280@gmail.com>
2021-10-16 23:09:45 +1100

    Add authentication failures to the interface of the package

--- auth_kit/core_auth_kit/lib/core_auth_kit.dart
+++ auth_kit/core_auth_kit/lib/core_auth_kit.dart
@@ -7,3 +7,4 @@ export 'package:core_auth_kit/src/presentation/bloc/log_in_bloc/log_in_bloc.dart
 /// Entites used by the blocs
 export 'package:core_auth_kit/src/domain/entities/core_auth_entities.dart';
 export 'package:core_auth_kit/src/domain/value_objects/core_auth_value_objects.dart';
+export 'package:core_auth_kit/src/core/failures/auth_failures.dart';
